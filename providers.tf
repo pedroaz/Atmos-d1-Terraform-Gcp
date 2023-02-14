fa85abab43667608f3ec7765e23dc3e630200765
Pedro Azevedo <pedro.azvm@gmail.com>
2023-02-14 13:46:20 -0300

    Commit from export: Export-3

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "google" {
+
+    credentials = file("gcp-service-account-credentials.json")
+    project     = var.project_id
+    region      = var.region
+    zone        = var.zone
+}
